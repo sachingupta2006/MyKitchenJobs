1768ac3351fe4075a23901b8c11ea987c77d552b
akhleshtiwari <akhleshtiwari499@gmail.com>
2024-06-13 18:03:15 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/my_kitchen_jobs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_kitchen_jobs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const primary = Color(0xFF0A93EB);
+  static const mustud = Color(0xFFF1C232);
+  static const grey = Color(0xFFF5F5F5);
+}
